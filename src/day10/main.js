df2e8fe3d4372be2b600906974e0b59225d183a9
Ashley Parks <parks.m.ashley@gmail.com>
2021-12-22 09:15:46 -0500

    day10 part2

--- src/day10/main.js
+++ src/day10/main.js
@@ -9,30 +9,76 @@ function read_file(name) {
 
 function syntax_error(navigation) {
 
-    var scores = {
+    var scores1 = {
         ')': 3,
         ']': 57,
         '}': 1197,
         '>': 25137
     }
+    
 
     let invalid_closes = []
+    let line_scores = [];
     for(let i =0; i<navigation.length; i++){
         let bad_close = find_first_invalid_close(navigation[i]);
         if(bad_close!=''){
             invalid_closes.push(bad_close);
         }else{
             //part2
-            
+            line_scores.push(find_missing_closes_score(navigation[i]))
         }
     }
     console.log("found bad closes: "+invalid_closes);
     let total_score = 0;
     for(let i= 0; i< invalid_closes.length; i++){
-        let score = scores[invalid_closes[i]];
+        let score = scores1[invalid_closes[i]];
         total_score+=score;
     }
-    console.log("error score: "+ total_score)
+    console.log("error score: "+ total_score);
+
+    
+    line_scores.sort((a,b)=> a-b);
+    console.log("line scores: "+ line_scores);
+    let index = Math.floor(line_scores.length/2);
+    console.log("index: "+ index)
+    console.log("middle line score: "+ line_scores[index])
+}
+
+function find_missing_closes_score(line) {
+    var open_to_close = {
+        '(': ')',
+        '[':']',
+        '{':'}',
+        '<':'>'
+    }
+    var scores2= {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    }
+    let lineScore = 0;
+    let openings = ['(','{','[','<'];
+    let openOrder = [];
+    let missing = [];
+    for(let i = 0; i<line.length; i++){
+        let character = line.charAt(i);
+        if(openings.includes(character)){
+            openOrder.push(character);
+        } else {
+            openOrder.pop();
+        }
+    }
+    while(openOrder.length > 0){
+        let openChar = openOrder.pop();
+        missing.push(open_to_close[openChar]);
+    }
+
+    for(let i = 0; i< missing.length; i++){
+        lineScore *=5
+        lineScore +=scores2[missing[i]];
+    }
+    return lineScore
 }
 
 function find_first_invalid_close(line){
